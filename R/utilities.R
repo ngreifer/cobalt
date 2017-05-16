e39602f9ae8499bfc99bbc51bdb21c4ecb876183
Noah Greifer <noah.greifer@gmail.com>
2017-05-15 22:27:02 -0400

    Added and.or option to word.list; created split.factor as a utility

--- R/utilities.R
+++ R/utilities.R
@@ -24,9 +24,10 @@ inxnoty <- function(x, y) {
     else out <- x[, is.na(match(X, Y))]
     return(out)
 }
-word.list <- function(word.list = NULL, and.or = "and") {
+word.list <- function(word.list = NULL, and.or = c("and", "or"), is.are = FALSE) {
     #When given a vector of strings, creates a string of the form "a and b"
     #or "a, b, and c"
+    #If is.are, adds "is" or "are" appropriately
     L <- length(word.list)
     if (L == 0) {
         out <- ""
@@ -39,27 +40,172 @@ word.list <- function(word.list = NULL, and.or = "and") {
         }
         else if (L == 1) {
             out <- word.list
+            if (is.are) out <- paste(out, "is")
         }
-        else if (L == 2) {
-            out <- paste(word.list, collapse = paste0(" ", and.or," "))
+        else {
+            and.or <- match.arg(and.or)
+            if (L == 2) {
+                out <- paste(word.list, collapse = paste0(" ", and.or," "))
+                if (is.are) out <- paste(out, "are")
+            }
+            else {
+                out <- paste(paste(word.list[seq_len(L-1)], collapse = ", "), 
+                             word.list[L], sep = paste0(", ", and.or," "))
+                
+            }
+            if (is.are) out <- paste(out, "are")
         }
-        else if (L >= 3) {
-            out <- paste(paste(word.list[seq_len(L-1)], collapse = ", "), 
-                         word.list[L], sep = paste0(", ", and.or," "))
-        }
+
             
     }
     return(out)
 }
-f.recode <- function(f, ...) {
-    #Simplified version of forcats::fct_recode
-    f <- factor(f)
-    new_levels <- unlist(list(...))
-    old_levels <- levels(f)
-    idx <- match(new_levels, old_levels)
+split.factor0 <- function(varname, data) {
+    #Splits factor into multiple (0, 1) indicators, replacing original factor in dataset. 
+    #Retains all categories.
+    #varname= the name of the variable to split when data is specified
+    #data=data set to be changed
     
-    old_levels[idx] <- names(new_levels)
+    x <- data[, varname] <- factor(data[, varname])
     
-    levels(f) <- old_levels
-    return(f)
+    if (nlevels(x) > 1) {
+        k <- model.matrix(as.formula(paste0("~", varname, "- 1")), data = data)
+    }
+    else {
+        k <- matrix(1, ncol = nlevels(x), nrow = length(x))
+    }
+    
+    if (is.null(levels(x))) colnames(k) <- paste0(varname, 1:ncol(k))
+    else colnames(k) <- paste(varname, substr(sapply(strsplit(colnames(k), varname), function(n) paste(n, collapse = "")), 1, 10), sep = "_")
+    
+    if (match(varname, names(data)) == 1){
+        data <- data.frame(k, data[, names(data)!=varname, drop = FALSE])
+    }
+    else if (match(varname, names(data)) == ncol(data)) {
+        data <- data.frame(data[, names(data)!=varname, drop = FALSE], k)
+    }
+    else {
+        where <- match(varname, names(data))
+        data <- data.frame(data[, 1:(where-1), drop = FALSE], k, data[, (where+1):ncol(data), drop = FALSE])
+    }
+    return(data)
+}
+split.factor <- function(data, varname, replace = TRUE, sep = "_", drop.first = c("if2", TRUE, FALSE), drop.singleton = FALSE) {
+    #Splits factor into multiple (0, 1) indicators, replacing original factor in dataset. 
+    #Retains all categories unless only 2 levels, in which case only the second level is retained.
+    #If variable only has one level, will delete.
+    #varname= the name of the variable to split when data is specified
+    #data=data set to be changed
+    
+    if (is.data.frame(data)) {
+        factor.names <- names(data)[sapply(data, is.factor)]
+        if (length(factor.names) == 0) {
+            stop("There are no factor variables to split in data.", call. = FALSE)
+        }
+        if (missing(varname)) {
+            varname <- factor.names
+        }
+        else if (is.character(varname)) {
+            if (any(varname %in% factor.names)) {
+                if (any(!varname %in% factor.names)) {
+                    not.in.factor.names <- varname[!varname %in% factor.names]
+                    warning(paste(word.list(not.in.factor.names, "and", is.are = TRUE), 
+                                  "not names of factor variables in data and will not be split."), 
+                            call. = FALSE)
+                }
+                varname <- varname[varname %in% factor.names]
+            }
+            else {
+                stop("No names in varname are names of factor variables in data.", call. = FALSE)
+            }
+        }
+        else {
+            stop("varname must be a character vector of the name(s) of factor variable(s) in data.", call. = FALSE)
+        }
+    }
+    else if (is.factor(data)) {
+        data <- data.frame(data)
+        if (missing(varname)) {
+            names(data) <- ""
+        }
+        else if (is.vector(varname) && (is.atomic(varname) || is.factor(varname))) {
+            if (length(varname) == 0) {
+                names(data) <- ""
+                sep <- ""
+            }
+            else if (length(varname) == 1) {
+                names(data) <- varname
+            }
+            else {
+                warning("Only using the first item of varname.", call. = FALSE)
+                names(data) <- varname[1]
+            }
+        }
+        else {
+            stop("varname must be an atmomic or factor vector of length 1 with the stem of the new variable.", call. = FALSE)
+        }
+        varname <- names(data)
+    }
+    else {
+        stop("data must a be a data.frame or a factor vector.", call. = FALSE)
+    }
+
+    for (v in varname) {
+        x <- data[, v] <- factor(data[, v])
+        
+        skip <- FALSE
+        if (nlevels(x) > 1) {
+            k <- model.matrix(as.formula(paste0("~", varname, "- 1")), data = data)
+        }
+        else {
+            if (drop.singleton) {
+                data <- data[, names(data)!=varname]
+                skip <- TRUE
+            }
+            else {
+                k <- matrix(1, ncol = 1, nrow = length(x))
+                #colnames(k) <- paste0(varname, levels(x)[1])
+            }
+        }
+        
+        if (!skip) {
+            
+            if (nlevels(x) == 0) colnames(k) <- paste0(varname, 1:ncol(k))
+            else colnames(k) <- paste(varname, sapply(strsplit(colnames(k), varname), function(n) paste(n, collapse = "")), sep = sep)
+            
+            if(ncol(k) == 2){
+                if (drop.first %in% c("if2", TRUE)) {
+                    k <- k[,-1, drop = FALSE]
+                }
+            } 
+            else if (ncol(k) > 2) {
+                if (drop.first == TRUE) {
+                    k <- k[,-1, drop = FALSE]
+                }
+            }
+            
+            if (ncol(data) == 1) {
+                data <- data.frame(k)
+            }
+            else if (replace) {
+                if (match(varname, names(data)) == 1){
+                    data <- data.frame(k, data[, names(data)!=varname, drop = FALSE])
+                }
+                else if (match(varname, names(data)) == ncol(data)) {
+                    data <- data.frame(data[, names(data)!=varname, drop = FALSE], k)
+                }
+                else {
+                    where <- match(varname, names(data))
+                    data <- data.frame(data[, 1:(where-1), drop = FALSE], k, data[, (where+1):ncol(data), drop = FALSE])
+                }
+            }
+            else {
+                data <- data.frame(data, k)
+            }
+
+        }
+
+    }
+
+    return(data)
 }
