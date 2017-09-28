5bf631e4cc47a39d9388a53c78a81fe48f4d0401
Noah Greifer <noah.greifer@gmail.com>
2017-09-28 01:16:01 -0400

    Updated page

--- R/do_not_include/Under_construction/under_construction.R
+++ R/do_not_include/Under_construction/under_construction.R
@@ -1,13 +1,5 @@
-#base.bal.tab.mi Generate balance tables for multiply imputed datasets. 
-#Choices to display balance for each one individually (somewhat similar to subclasses) or overall summary
-#Overall summary might be max/min/mean/median statistics for each covariate
-#Should be able to take arguments either as single objects to apply to all or multiple objects in lists, 
-#or demarcated with imputation indicators (e.g., output from MICE). 
-
 skew.diff <- function(x, group, weights, var.type) {
-    #Currently unused
     #Calculates weighted skew and skew difference for groups; uses formulas at http://www.nematrian.com/WeightedMomentsAndCumulants
-    #Weighted variance is different from one used for std.diff.
     skew.cols <- data.frame(Skew.C = NA, Skew.T = NA, Skew.Diff = NA)
     if (is.null(weights)) weights <- rep(1, length(x))
     if (var.type=="Contin.") {
@@ -35,30 +27,3 @@ skew.diff <- function(x, group, weights, var.type) {
     return(skew.cols)
 }
 
-dum2factor <- function(data, varlist, newvar, other = NULL, sep = NULL) {
-    #Like splitfactor; creates factor variable from series of dummies. Could be used as a utility.
-    dummies <- data[, varlist]
-    if (sum(rowSums(dummies)) < nrow(dummies)) dummies <- setNames(data.frame(dummies, ifelse(rowSums(dummies)==0, 1, 0)),c(names(dummies), other))
-    factor <- factor(as.matrix(dummies) %*% 1:ncol(dummies), labels = colnames(dummies))
-    
-    if (!is.null(sep)) {
-        if (sep=="") sep <- newvar
-        levels(factor) <- unlist(strsplit(colnames(dummies), sep))[2 * (1:ncol(dummies))]
-    }
-    
-    if (match(varlist[1], names(data)) == 1){
-        data <- data.frame(factor, data[, !names(data) %in% varlist, drop = FALSE])
-        names(data)[1] <- newvar
-    }
-    else if (match(varlist[length(varlist)], names(data)) == ncol(data)) {
-        data <- data.frame(data[, !names(data) %in% varlist, drop = FALSE], factor)
-        names(data)[ncol(data)] <- newvar
-    }
-    else {
-        where.a <- match(varlist[1], names(data))
-        where.b <- match(varlist[length(varlist)], names(data))
-        data <- data.frame(data[, 1:(where.a-1), drop = FALSE], factor, data[, (where.b+1):ncol(data), drop = FALSE])
-        names(data)[where.a] <- newvar
-    }
-    return(data)
-}
