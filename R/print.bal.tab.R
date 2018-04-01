fc17d38aa779572a1aeff6f361c06fcf0c3ccb8e
Noah Greifer <noah.greifer@gmail.com>
2018-04-01 15:09:21 -0400

    Fixed bug with maximum imbalances

--- R/print.bal.tab.R
+++ R/print.bal.tab.R
@@ -386,7 +386,7 @@ print.bal.tab.subclass <- function(x, disp.m.threshold = "as.is", disp.v.thresho
     }
     if (!is.null(maximbal.r.subclass)) {
         cat("Variable with the greatest treatment correlation across subclasses:\n")
-        print.data.frame(round_df(maximbal.r.subclass, digits), row.names = FALSE)
+        print.data.frame(round_df(maximbal.r.subclass, digits), row.names = TRUE)
         cat("\n")
     }
     if (!is.null(baltal.m.subclass)) {
@@ -396,7 +396,7 @@ print.bal.tab.subclass <- function(x, disp.m.threshold = "as.is", disp.v.thresho
     }
     if (!is.null(maximbal.m.subclass)) {
         cat("Variable with the greatest mean difference across subclasses:\n")
-        print.data.frame(round_df(maximbal.m.subclass, digits), row.names = FALSE)
+        print.data.frame(round_df(maximbal.m.subclass, digits), row.names = TRUE)
         cat("\n")
     }
     if (!is.null(baltal.v.subclass)) {
@@ -406,7 +406,7 @@ print.bal.tab.subclass <- function(x, disp.m.threshold = "as.is", disp.v.thresho
     }
     if (!is.null(maximbal.v.subclass)) {
         cat("Variable with the greatest variance ratios across subclasses:\n")
-        print.data.frame(round_df(maximbal.v.subclass, digits), row.names = FALSE)
+        print.data.frame(round_df(maximbal.v.subclass, digits), row.names = TRUE)
         cat("\n")
     }
     if (!is.null(baltal.ks.subclass)) {
@@ -416,7 +416,7 @@ print.bal.tab.subclass <- function(x, disp.m.threshold = "as.is", disp.v.thresho
     }
     if (!is.null(maximbal.ks.subclass)) {
         cat("Variable with the greatest KS statistc across subclasses:\n")
-        print.data.frame(round_df(maximbal.ks.subclass, digits), row.names = FALSE)
+        print.data.frame(round_df(maximbal.ks.subclass, digits), row.names = TRUE)
         cat("\n")
     }
     
