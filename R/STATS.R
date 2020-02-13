46f84ae7e5b120183d90a46845b362557120572e
Noah Greifer <noah.greifer@gmail.com>
2020-02-13 02:49:28 -0500

    Added STATS to main folder

---
+++ R/STATS.R
@@ -0,0 +1,179 @@
+#Trying to make a general stats object
+
+get_from_STATS <- function(what) {
+    setNames(sapply(STATS, function(s) s[[what]], USE.NAMES = FALSE),
+             names(STATS))
+}
+
+STATS <- list()
+
+STATS[["mean.diffs"]] <- {list(
+    type = "bin",
+    threshold = "m.threshold",
+    Threshold = "M.Threshold",
+    disp_stat = "disp.diff",
+    bin_only = FALSE,
+    abs = function(x) abs_(x),
+    bal.tab._column_prefix = "Diff", #Also which.stat in love.plot
+    threshold_range = c(0, Inf),
+    balance_tally_for = "mean differences",
+    variable_with_the_greatest = "mean difference", #also which.stat2 in love.plot
+    love.plot_xlab = function(...) {
+        A <- list(...)
+        binary <- A$binary #attr(x, "print.options")$binary
+        continuous <- A$continuous #attr(x, "print.options")$continuous
+        abs <- A$abs
+        var_type <- A$var_type #B[["type"]]
+        stars <- A$stars
+        
+        #All std, no std, some std
+        if ((binary == "std" || sum(var_type == "Binary") == 0) && 
+            (continuous == "std" || sum(var_type != "Binary") == 0)) {
+            xlab.diff <- "Standardized Mean Differences"
+        } 
+        else if ((binary == "raw" || sum(var_type == "Binary") == 0) && 
+                 (continuous == "raw" || sum(var_type != "Binary") == 0)) {
+            xlab.diff <- "Mean Differences"
+        }
+        else {
+            stars <- match_arg(stars, c("none", "std", "raw"))
+            if (stars == "none") {
+                xlab.diff <- "Mean Differences"
+            }
+            else if (stars == "std") {
+                xlab.diff <- "Mean Differences"
+            }
+            else if (stars == "raw") {
+                xlab.diff <- "Standardized Mean Differences"
+            }
+        }
+        
+        xlab <- if (abs) paste("Absolute", xlab.diff) else xlab.diff
+        return(xlab)
+    },
+    love.plot_add_stars = function(SS.var, variable.names, ...) {
+        A <- list(...)
+        binary <- A$binary #attr(x, "print.options")$binary
+        continuous <- A$continuous #attr(x, "print.options")$continuous
+        var_type <- A$var_type #B[["type"]]
+        stars <- A$stars
+        star_char = A$star_char #args$star_char
+        
+        
+        #All std, no std, some std
+        if (!((binary == "std" || sum(var_type == "Binary") == 0) && 
+              (continuous == "std" || sum(var_type != "Binary") == 0)) 
+            &&
+            !((binary == "raw" || sum(var_type == "Binary") == 0) && 
+              (continuous == "raw" || sum(var_type != "Binary") == 0))) {
+            
+            stars <- match_arg(stars, c("none", "std", "raw"))
+            if (stars == "none") {
+                warning("Standardized mean differences and raw mean differences are present in the same plot. \nUse the 'stars' argument to distinguish between them and appropriately label the x-axis.", call. = FALSE)
+            }
+            else {
+                if (length(star_char) != 1 || !is.character(star_char)) star_char <- "*"
+                
+                vars_to_star <- setNames(rep(FALSE, length(variable.names)), variable.names)
+                if (stars == "std") {
+                    if (binary == "std") vars_to_star[variable.names[var_type == "Binary"]] <- TRUE
+                    if (continuous == "std") vars_to_star[variable.names[var_type != "Binary"]] <- TRUE
+                }
+                else if (stars == "raw") {
+                    if (binary == "raw") vars_to_star[variable.names[var_type == "Binary"]] <- TRUE
+                    if (continuous == "raw") vars_to_star[variable.names[var_type != "Binary"]] <- TRUE
+                }
+                new.variable.names <- setNames(variable.names, variable.names)
+                names(new.variable.names)[vars_to_star[variable.names]] <- paste0(variable.names[vars_to_star[variable.names]], star_char)
+                SS.var <- do.call(f.recode, c(list(SS.var), new.variable.names))
+            }
+        }
+        
+        return(SS.var)
+    },
+    baseline.xintercept = 0,
+    threshold.xintercepts = function(threshold, abs) {
+        if (abs) c(lower = base::abs(threshold))
+        else c(lower = -base::abs(threshold), upper = base::abs(threshold))
+    },
+    love.plot_axis_scale = ggplot2::scale_x_continuous
+)}
+
+STATS[["variance.ratios"]] <- {list(
+    type = "bin",
+    threshold = "v.threshold",
+    Threshold = "V.Threshold",
+    disp_stat = "disp.v.ratio",
+    bin_only = TRUE,
+    abs = function(x) abs_(x, ratio = TRUE),
+    bal.tab._column_prefix = "V.Ratio", #Also which.stat in love.plot
+    threshold_range = c(1, Inf),
+    balance_tally_for = "variance ratios",
+    variable_with_the_greatest = "variance ratio", #also which.stat2 in love.plot
+    love.plot_xlab = function(...) {
+        "Variance Ratios"
+    },
+    love.plot_add_stars = function(SS.var, variable.names, ...) {
+        return(SS.var)
+    },
+    baseline.xintercept = 1,
+    threshold.xintercepts = function(threshold, abs) {
+        if (abs) c(lower = abs_(threshold, ratio = TRUE))
+        else c(lower = abs_(threshold, ratio = TRUE)^-1, upper = abs_(threshold, ratio = TRUE))
+    },
+    love.plot_axis_scale = ggplot2::scale_x_log10
+)}
+
+STATS[["ks.statistics"]] <- {list(
+    type = "bin",
+    threshold = "ks.threshold",
+    Threshold = "KS.Threshold",
+    disp_stat = "disp.ks",
+    bin_only = FALSE,
+    abs = function(x) abs_(x),
+    bal.tab._column_prefix = "KS", #Also which.stat in love.plot
+    threshold_range = c(0, 1),
+    balance_tally_for = "KS statistics",
+    variable_with_the_greatest = "KS statistic", #also which.stat2 in love.plot
+    love.plot_xlab = function(...) {
+        "Kolmogorov-Smirnov Statistics"
+    },
+    love.plot_add_stars = function(SS.var, variable.names, ...) {
+        return(SS.var)
+    },
+    baseline.xintercept = 0,
+    threshold.xintercepts = function(threshold, abs) {
+        c(lower = base::abs(threshold))
+    },
+    love.plot_axis_scale = ggplot2::scale_x_continuous
+)}
+
+STATS[["correlations"]] <- {list(
+    type = "cont",
+    threshold = "r.threshold",
+    Threshold = "R.Threshold",
+    disp_stat = "disp.corr",
+    bin_only = FALSE,
+    abs = function(x) abs_(x),
+    bal.tab._column_prefix = "Corr", #Also which.stat in love.plot
+    threshold_range = c(0, 1),
+    balance_tally_for = "treatment correlations",
+    variable_with_the_greatest = "treatment correlation", #also which.stat2 in love.plot
+    love.plot_xlab = function(...) {
+        A <- list(...)
+        abs <- A$abs
+        if (abs) return("Absolute Treatment-Covariate Correlations")
+        else return("Treatment-Covariate Correlations")
+    },
+    love.plot_add_stars = function(SS.var, variable.names, ...) {
+        return(SS.var)
+    },
+    baseline.xintercept = 0,
+    threshold.xintercepts = function(threshold, abs) {
+        if (abs) c(lower = base::abs(threshold))
+        else c(lower = -base::abs(threshold), upper = base::abs(threshold))
+    },
+    love.plot_axis_scale = ggplot2::scale_x_continuous
+)}
+
+all_STATS <- names(STATS)
