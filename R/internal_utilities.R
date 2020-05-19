ab8db4a8c37dfc9940e4fec4c22c26fbc1d3d4bd
Noah Greifer <noah.greifer@gmail.com>
2020-05-19 03:47:47 -0400

    Deleted

--- R/internal_utilities.R
+++
@@ -1,5 +0,0 @@
-#Internal Utilities not in SHARED.R
-`%+%` <- function(...) {
-    if (is_(..1, c("atomic", "factor")) && is_(..2, c("atomic", "factor"))) crayon::`%+%`(as.character(..1), as.character(..2))
-    else ggplot2::`%+%`(...)
-}
