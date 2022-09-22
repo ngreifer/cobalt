c3a3219cd71e6e59e723141932f36ab44f8889f4
Noah Greifer <noah.greifer@gmail.com>
2022-09-22 12:49:07 -0400

    Added testing

---
+++ tests/testthat/test-bal.tab.data.frame.R
@@ -0,0 +1,17 @@
+test_that("bal.tab() works with data.frames", {
+    data("lalonde")
+    cov_names <- c("age", "educ", "race", "married", "nodegree", "re74", 
+                   "re75")
+    covs <- lalonde[cov_names]
+    w <- runif(nrow(lalonde))
+    sw <- runif(nrow(lalonde))
+    dist <- runif(nrow(lalonde))
+    
+    expect_s3_class(bal.tab(covs, treat = lalonde$treat), "bal.tab")
+    expect_s3_class(bal.tab(covs, treat = lalonde$treat, distance = dist,
+                            weights = w, s.weights = sw), "bal.tab")
+    expect_s3_class(bal.tab(covs, treat = lalonde$treat, distance = dist,
+                            weights = w, s.weights = sw, cluster = lalonde$race), "bal.tab.cluster")
+    expect_s3_class(bal.tab(covs, treat = lalonde$race, distance = dist,
+                            weights = w, s.weights = sw), "bal.tab.multi")
+})
---
+++ tests/testthat/test-bal.tab.weightit.R
@@ -0,0 +1,26 @@
+test_that("bal.tab() works with WeightIt", {
+    data("lalonde")
+    cov_names <- c("age", "educ", "race", "married", "nodegree", "re74", 
+                   "re75")
+    sw <- runif(nrow(lalonde))
+    
+    w <- WeightIt::weightit(treat ~age + educ + race + married + nodegree + re74 + re75,
+                            data = lalonde)
+    
+    expect_s3_class(bal.tab(w), "bal.tab")
+    
+    w <- WeightIt::weightit(race ~age + educ + married + nodegree + re74 + re75,
+                            data = lalonde)
+    
+    expect_s3_class(bal.tab(w), "bal.tab.multi")
+    
+    w <- WeightIt::weightit(re75 ~age + educ + married + nodegree + re74,
+                            data = lalonde)
+    
+    expect_s3_class(bal.tab(w), "bal.tab.cont")
+    
+    w <- WeightIt::weightit(treat ~ age + splines::ns(educ, 3) + race + married + nodegree + re74 + re75,
+                            data = lalonde)
+    
+    expect_s3_class(bal.tab(w), "bal.tab")
+})
