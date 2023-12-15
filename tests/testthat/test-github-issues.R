636ffd3ff6c93dee0f8f129bb4e9dbaeeaf8b2c6
Noah Greifer <noah.greifer@gmail.com>
2023-12-15 14:46:11 -0500

    Added tests for specific github issues

---
+++ tests/testthat/test-github-issues.R
@@ -0,0 +1,65 @@
+#Tests for specific github issues
+
+test_that("(#71) `addl` argument not throwing correct error when variable not available", {
+    #Note: actual error was something else, but addl should have thrown a more informative error
+    skip_if_not_installed("MatchIt")
+    
+    data("lalonde")
+    
+    m <- MatchIt::matchit(treat ~ age + educ, data = lalonde, distance = "scaled_euclidean")
+    
+    expect_error(bal.tab(m, addl = "race"),
+                 "The variable \"race\" cannot be found. Be sure it is entered correctly or supply a dataset that contains this varialble to `data`.",
+                 fixed = TRUE)
+    
+    expect_no_condition(bal.tab(m, addl = "race", data = lalonde))
+ })
+
+test_that("(#74) bal.tab() works with mnps objects", {
+    skip_if_not_installed("twang")
+    
+    data("lalonde")
+    
+    mnps <- twang::mnps(race ~ age + educ + married + re74, 
+                        data = lalonde,
+                        estimand = "ATT",
+                        treatATT = "white",
+                        verbose = FALSE,
+                        stop.method = c("es.mean"), 
+                        n.trees = 1000)
+    
+    expect_no_condition(
+        bal.tab(mnps, un = TRUE, disp = c("m", "sd"))
+    )
+})
+
+test_that("(#76) bal.tab() doesn't produce an error with missing covariates", {
+    
+    data("lalonde_mis")
+    
+    expect_no_error(
+        b <- bal.tab(treat ~ age + educ + race + married + nodegree + re74 + re75,
+                     data = lalonde_mis)
+    )
+    expect_warning(
+        b <- bal.tab(treat ~ age + educ + race + married + nodegree + re74 + re75,
+                     data = lalonde_mis),
+        "Missing values exist in the covariates. Displayed values omit these observations.",
+        fixed = TRUE
+    )
+    
+    expect_identical(rownames(b$Balance),
+                     c("age", "educ", "race", "married", "married:<NA>", "nodegree",
+                       "re74", "re74:<NA>", "re75", "re75:<NA>"))
+})
+
+test_that("(#77) no conflict with `cluster` argument to bal.tab() when {caret} is loaded", {
+    skip_if_not_installed("caret")
+    
+    library(caret)
+    
+    data("lalonde")
+    
+    expect_no_condition(bal.tab(treat ~ age + educ + race, data = lalonde, cluster = "married",
+                                s.d.denom = "pooled"))
+})
